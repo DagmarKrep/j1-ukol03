4908d1baa6dc474d164512f50658aceddc0f27e0
Dasa Krepsova <dagmar.krepsova@seznam.cz>
2023-03-17 13:35:55 +0100

    Ukol c.3 - cast 1

--- src/main/java/cz/czechitas/ukol3/HlavniProgram.java
+++ src/main/java/cz/czechitas/ukol3/HlavniProgram.java
@@ -12,10 +12,10 @@ public class HlavniProgram {
 
     public static void main(String... args) {
 
-//        Pocitac mujPocitac;
-//        mujPocitac = new Pocitac();
-//        System.out.println(mujPocitac.toString());
-//        mujPocitac.zapniSe();
+        Pocitac mujPocitac;
+        mujPocitac = new Pocitac();
+        System.out.println(mujPocitac.toString());
+        mujPocitac.zapniSe();
 
         Disk mujDisk = new Disk();
         mujDisk.setKapacitaDisku(1_099_511_627_776L);
@@ -28,18 +28,29 @@ public class HlavniProgram {
         mujProcesor.setVyrobce("Intel");
         mujProcesor.setRychlost(3_600_000_000L);
 
-        System.out.println("Program spuštěn.");
+        mujPocitac.setCpu(mujProcesor);
+        mujPocitac.setRam(mojePamet);
+        mujPocitac.setPevnyDisk(mujDisk);
 
+        System.out.println(mujPocitac.toString());
+
+        mujPocitac.zapniSe();
+        mujPocitac.zapniSe();
+
+        System.out.println(mujPocitac.toString());
+
+        System.out.println("Program spuštěn.");
         System.out.println("Kapacita disku: " + mujDisk.getKapacitaDisku() + " B ");
         System.out.println("Vyuzite misto na disku: " + mujDisk.getVyuziteMisto() + " B ");
         System.out.println(mojePamet);
         System.out.println("Vyrobce: " + mujProcesor.getVyrobce());
         System.out.println("Rychlost: " + mujProcesor.getRychlost() + " Hz ");
 
+        mujPocitac.vypnise();
+        mujPocitac.vypniSe();
+        mujPocitac.vypniSe();
 
         //TODO tady bude váš kód - vytvoření instance třídy Pocitac, zapnutí, vpynutí, výpis hodnot.
-
-
 
     }
 
--- src/main/java/cz/czechitas/ukol3/Pocitac.java
+++ src/main/java/cz/czechitas/ukol3/Pocitac.java
@@ -2,8 +2,10 @@ package cz.czechitas.ukol3;
 
 public class Pocitac {
 
-    private boolean jeZapnuty;
+    private static boolean jeZapnuty;
 
+    private boolean jeVypnuty;
+    
     private Procesor cpu;
 
     private Pamet ram;
@@ -33,6 +35,52 @@ public class Pocitac {
 
     public void setPevnyDisk(Disk pevnyDisk) {
         this.pevnyDisk = pevnyDisk;
+    }
+
+    public boolean isJeZapnuty() {
+        return this.jeZapnuty;
+    }
+
+    public void setJeZapnuty(boolean jeZapnuty) {
+        this.jeZapnuty = jeZapnuty;
+    }
+
+    public void zapniSe() {
+        setJeZapnuty( boolean Pocitac.jeZapnuty) {
+            this.jeZapnuty = jeZapnuty;
+            System.out.println("Pocitac je zapnuty.");
+        }
+    }
+
+    public boolean isJeVypnuty () {
+        return this.jeVypnuty;
+    }
+
+    public void setJeVypnuty ( boolean jeVypnuty) {
+        this.jeVypnuty = jeVypnuty;
+    }
+
+    public void VypniSe () {
+        setJeVypnuty( boolean jeVypnuty) {
+            this.jeVypnuty = jeVypnuty;
+            System.out.println("Pocitac je vypnuty.");
+        }
+    }
+
+ if (Procesor == null) {
+        System.err.println("Chybi procesor.");
+        return;
+    } else if (Pamet == null) {
+        System.err.println("Chybi pamet.");
+        return;
+    } else if (Disk == null) {
+        System.err.println("Chybi disk.");
+        return;
+    } else if (Pocitac.jeZapnuty) {
+        System.err.println("Pocitac je zapnuty.");
+        return;
+    } else {
+     zapniSe();
     }
 
 }
